0a1d4692a51db02bf4f2948923a9b3840b0bc386
System Administrator <root@Kunals-MacBook-Air.local>
2025-07-08 20:25:49 +0530

    first commit

---
+++ lib/src/core/constants/assets.dart
@@ -0,0 +1,26 @@
+class Links {
+  static const String sourceCode =
+      'https://github.com/ShokhrukhbekYuldoshev/Meloplay';
+}
+
+class Assets {
+  // Icons
+  static const String logo = 'assets/icon/icon.png';
+
+  // Images
+  static const String earphones = 'assets/images/earphones.jpg';
+  static const String heart = 'assets/images/heart.jpg';
+
+  // Lottie animations
+  static const String playingAnimation = 'assets/lottie/playing.json';
+  static const String scanningAnimation = 'assets/lottie/scanning.json';
+
+  // SVG
+  static const String menuSvg = 'assets/svg/menu.svg';
+  static const String playSvg = 'assets/svg/play.svg';
+  static const String pauseSvg = 'assets/svg/pause.svg';
+  static const String nextSvg = 'assets/svg/next.svg';
+  static const String previousSvg = 'assets/svg/previous.svg';
+  static const String shuffleSvg = 'assets/svg/shuffle.svg';
+  static const String settingsSvg = 'assets/svg/settings.svg';
+}
